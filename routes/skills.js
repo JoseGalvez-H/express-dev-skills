7da096fa90d05aaca55da7a22dbfea0e41a06f24
Jose Galvez-Hernandez <josemgalvezh90@gmail.com>
2024-03-31 21:38:57 -0700

    implement create and delete functionality

--- routes/skills.js
+++ routes/skills.js
@@ -3,7 +3,12 @@ var router = express.Router();
 var skillsCtrl = require('../controllers/skills');
 
 router.get('/', skillsCtrl.index);
-
+// GET /skills/new <-- define before show route
+router.get('/new', skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
+// POST /skills
+router.post('/', skillsCtrl.create);
+// Add delete route
+router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
